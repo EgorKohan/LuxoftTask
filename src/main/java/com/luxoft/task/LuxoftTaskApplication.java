291797cc0933b1c69c430085672590906de3f9b5
Yahor Kokhan <Yahor.Kokhan@solera.com>
2023-02-09 15:25:19 +0100

    Initial commit

---
+++ src/main/java/com/luxoft/task/LuxoftTaskApplication.java
@@ -0,0 +1,13 @@
+package com.luxoft.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxoftTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuxoftTaskApplication.class, args);
+    }
+
+}
